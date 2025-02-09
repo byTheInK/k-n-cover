4ee08087912c9322f9a09ccf31dd1c0f3395b51e
byTheInK <un.known.river.person@gmail.com>
2025-02-09 22:18:18 +0300

    Added GNOME/GTK themes

--- cover.sh
+++ cover.sh
@@ -1,9 +1,9 @@
 FILE="$HOME/.config/kdeglobals"
-ICON_THEME="Win11-dark" // Set this to Win11-dark or We10X-black
+ICON_THEME="Win11-dark"
 REGULAR_WALLPAPER="/usr/share/wallpapers/ScarletTree/contents/images_dark/5120x2880.png"
-// Change the regular wallpaper variable to the path of your image
 UNDERCOVER_WALLPAPER="$HOME/windows11wallpaper.jpg"
-// Windows 11 wallpaper image
+REGULAR_GTK_THEME="Breeze"
+UNDERCOVER_GTK_THEME="Windows-10-3.2.1"
 
 if [ ! -f /var/tmp/kdeglobals ]; then
   echo "/var/tmp/kdeglobals not found"
@@ -11,10 +11,12 @@ if [ ! -f /var/tmp/kdeglobals ]; then
 
   sed -i "/^\[Icons\]/,/^\[/ s/^Theme=.*/Theme=$ICON_THEME/" "$FILE"
   plasma-apply-wallpaperimage $UNDERCOVER_WALLPAPER
+  dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:$UNDERCOVER_GTK_THEME"
 else
   echo "/var/tmp/kdeglobals found"
   mv /var/tmp/kdeglobals $FILE
   plasma-apply-wallpaperimage $REGULAR_WALLPAPER
+  dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:$REGULAR_GTK_THEME"
 fi
 
 kquitapp5 plasmashell && kstart5 plasmashell
