30e26a623034c5e2109864873b66a9ba73312b63
byTheInK <un.known.river.person@gmail.com>
2025-02-09 21:15:54 +0300

    Added comments for configuration

--- cover.sh
+++ cover.sh
@@ -1,7 +1,9 @@
 FILE="$HOME/.config/kdeglobals"
-ICON_THEME="Win11-dark"
+ICON_THEME="Win11-dark" // Set this to Win11-dark or We10X-black
 REGULAR_WALLPAPER="/usr/share/wallpapers/ScarletTree/contents/images_dark/5120x2880.png"
+// Change the regular wallpaper variable to the path of your image
 UNDERCOVER_WALLPAPER="$HOME/windows11wallpaper.jpg"
+// Windows 11 wallpaper image
 
 if [ ! -f /var/tmp/kdeglobals ]; then
   echo "/var/tmp/kdeglobals not found"
